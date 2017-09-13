44d23949d1bcfbae92e7684e52d435d36f79a1be
Dmitry Kutsenko <d2emonium@gmail.com>
2017-09-13 20:07:46 +0300

    More modules

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,8 +1,13 @@
+# from flask import Flask, render_template
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+# from flask_bootstrap import Bootstrap
+from flask_cache import Cache
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_script import Manager
-from config import BASE_DIR, app_config
+from flask_sqlalchemy import SQLAlchemy
+from config import app_config
 
 
 import logging
@@ -21,24 +26,21 @@ def create_logger(log_config=dict()):
 
 
 def create_app(debug=False, config_name='production'):
-    app = Flask(__name__)
-    app.config.from_object(app_config[config_name])
-    app.static_folder = os.path.join(BASE_DIR, app.config.get('STATIC_FOLDER', 'static'))
-    app.template_folder = os.path.join(BASE_DIR, app.config.get('TEMPLATE_FOLDER', 'templates'))
-    
-    # Session(app)
+    app = Flask(__name__, instance_relative_config=True)
 
-    db = SQLAlchemy(app)
-    db.create_all()
+    # Loading config
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    # app.config.from_envvar('FLASK_CONFIG_FILE')
+    app.static_folder = app.config.get('STATIC_FOLDER', 'static')
+    app.template_folder = app.config.get('TEMPLATE_FOLDER', 'templates')
 
     log_config = app.config.get("LOG", dict())
     app.logger.addHandler(create_logger(log_config))
 
-    migrate = Migrate(app, db)
-
     # from execom import models
 
-    return app, db
+    return app
 
 
 import os
@@ -46,10 +48,41 @@ import os
 
 debug = os.environ.get('FLASK_DEBUG', False)
 config_name = os.environ.get('FLASK_CONFIG', 'production')
-app, db = create_app(config_name=config_name)
+app = create_app(config_name=config_name)
+
+# bootstrap = Bootstrap(app)
+
+cache = Cache(app)
+
+toolbar = DebugToolbarExtension(app)
+
+login_manager = LoginManager(app)
+# login_manager.login_message = "You must be logged in to access this page."
+# login_manager.login_view = "auth.login"
+login_manager.login_view = 'login'
+
 manager = Manager(app)
 
+db = SQLAlchemy(app)
+db.create_all()
 
-from execom.commands import *
-from execom.views import *
-from case.views import *
+migrate = Migrate(app, db)
+
+# Session(app)
+
+
+# from blog import models
+
+
+# from .admin import admin as admin_blueprint
+# app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+# from .auth import auth as auth_blueprint
+# app.register_blueprint(auth_blueprint)
+
+# from .home import home as home_blueprint
+# app.register_blueprint(home_blueprint)
+
+
+from app.views import *
+# from blog.views import *
